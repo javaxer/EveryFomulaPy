b1137f5df0e4e38dfdf72a538bb377f9eeff309d
javaxer <javaxer@gmail.com>
2018-01-21 23:44:20 +0900

    버그 내용 수정(키워드를 잘못 적지 않나 아주 날리도 아니었다)

--- classicphysics/mechanics/measurement.py
+++ classicphysics/mechanics/measurement.py
@@ -2,7 +2,8 @@
 
 #길이의 단위와 그 전환에 대한 정의
 #내용은 모두 Serway물리 교과서 내용에서 발췌했음
-#처음에는 파일명을 Length라고 했으나 곰곰히 생각해 본 결과 정의(Standard)라고 정의하는 쪽이 더 정확할 것으로 보여 파일 명을 수정함.
+#처음에는 파일명을 Length라고 했으나 곰곰히 생각해 본 결과 정의(Standard)라고 정의하는 쪽이 더 정확할 것으로 보여
+# 파일 명을 수정함.
 
 import math
 
@@ -11,10 +12,10 @@ class Standard:
     #생성자 정의
     def __init__(self, number=0.0 ):
         self.standard = number
-    def __init__(self):
-        self.standard = 0.0
-    def __init__(self,number):
-        self.standard = number
+#    def __init__(self):
+#        self.standard = 0.0
+#    def __init__(self,number):
+#        self.standard = number
 
 
     def get_definition(self):
@@ -30,13 +31,15 @@ class Standard:
 #SI단위계 표준 단위인 미터에 대한 정의
 class Length(Standard):
     # 1m에 대한 물리적인 정의
-    speed_of_light = 299,792,458
+    speed_of_light = 299792458
     def get_definition(self):
         print("1미터(m)는 진공 속에서 빛이 1/299,792,458초 동안 진행한 거리이다.")
     def set_length(self,length):
         self.set(length)
     def get_lenght(self):
         return self.get()
+    def get_speed_of_light(self):
+        return self.speed_of_light
 
 #아래의 클래스는 위 길이에 대한 클래스의 한글 클래스이다.
 class 길이(Length):
@@ -78,14 +81,14 @@ class 길이(Time):
         self.get()   
 
 
-class Volume:
+class Volume(Standard):
     #부피에 대한 정의, 기초적인 산수 영역이나 아래의 밀도에 대한 정의를 위해 추가한다.
+
     #정육면체의 부피에 대한 공식
-    def __init__(self,volume=0):
-        self.volume = volume
     def get_square_volume(self,length):
         return math.pow(length.get(),3)
-    #직육면체에 부피에 대한 공식
+
+    # 직육면체에 부피에 대한 공식
     def get_rectengular_volume(self,length_x, length_y, length_z):
         return length_x*length_y*length_z
 
@@ -94,14 +97,16 @@ class 부피(Volume):
         self.set(number)
     def 획득(self,number):
         self.get(number)
+    def 직육면체부피(self,length):
+        self.get()
 
 class Density:
     def getdensity(self, mass, volume):
-        return m/v
+        return mass/volume
 
-class 밀도(Desity):
-    def 획득(self,,number):
-        self.set(number)
+class 밀도(Density):
+    def 획득(self,mass, volume):
+        return self.getdensity(mass,volume)
 
 class DimensionalAnalysis:
     #차원 분석에 대한 클래스, 현시점에서는 검증용 외에는 별다른 용도가 없으므로 일단 통과하자
